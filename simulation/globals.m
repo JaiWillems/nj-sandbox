8485adddcabfb0e4d24a7a21d6c48768458b8fc3
Nishant Kumar <nishanika3@gmail.com>
2026-02-02 16:56:59 -0500

    Adding the x,y,z subsystem

--- simulation/globals.m
+++ simulation/globals.m
@@ -1,5 +1,5 @@
 
-m = 0.547; % [kg], drone mass.
+m = 0.061; % [kg], drone mass.
 L = 0.17; % [m], arm length.
 
 Ixb = 0.0033; % [kg m^2], body x moment of inertia.
@@ -8,3 +8,5 @@ Izb = 0.0058; % [kg m^2], body z moment of inertia.
 
 kF = 0.00000015; % [N/RPM^2], motor force coefficient.
 kM = 0.000000375; % [Nm/RPM^2], motor torque coefficient.
+
+g = 9.81; % [m/s^2], gravitational acceleration
