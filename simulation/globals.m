c1addeb7e57d6b5d84e3e50cf4dbcc154494355c
Jai Willems <jai52h@hotmail.com>
2026-02-06 21:58:39 +1000

    Correct small bug with z equation of motion.

--- simulation/globals.m
+++ simulation/globals.m
@@ -1,12 +1,12 @@
 
-m = 0.061; % [kg], drone mass.
+m = 0.547; % [kg], drone mass.
 L = 0.17; % [m], arm length.
 
 Ixb = 0.0033; % [kg m^2], body x moment of inertia.
 Iyb = 0.0033; % [kg m^2], body y moment of inertia.
 Izb = 0.0058; % [kg m^2], body z moment of inertia.
 
-kF = 0.00000015; % [N/RPM^2], motor force coefficient.
-kM = 0.000000375; % [Nm/RPM^2], motor torque coefficient.
+kF = 0.00000015; % [N / RPM^2], motor force coefficient.
+kM = 0.000000375; % [Nm / RPM^2], motor torque coefficient.
 
-g = 9.81; % [m/s^2], gravitational acceleration
+g = 9.81; % [m / s^2], gravitational acceleration.
