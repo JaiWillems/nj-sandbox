5e8f10b8daf79746f3018535e3961de33dd2fa17
Jai Willems <jai52h@hotmail.com>
2026-02-28 11:25:44 +1000

    Partial tuning complete, added physical constraints.

--- simulation/globals.m
+++ simulation/globals.m
@@ -1,28 +1,35 @@
 
 g = 9.81; % [m / s^2], gravitational acceleration.
 
-m = 0.547; % [kg], drone mass.
-L = 0.17; % [m], arm length.
+m = 1; % [kg], drone mass.
+L = 0.27305; % [m], arm length.
 
 Ixb = 0.0033; % [kg m^2], body x moment of inertia.
 Iyb = 0.0033; % [kg m^2], body y moment of inertia.
-Izb = 0.0058; % [kg m^2], body z moment of inertia.
+Izb = 0.0058; % [kg m^2], body z moment of inertia. 
 
-kF = 0.00000015; % [N / RPM^2], motor force coefficient.
-kM = 0.000000375; % [Nm / RPM^2], motor torque coefficient.
+r = 2; % Thrust to weight ratio.
+kF = r * m * g / (4 * 100); % [N / PWM], motor force coefficient.
 
-z_Kp = 1;
-z_Ki = 1;
-z_Kd = 1;
+I = 30; % [A], ESC current.
+kV = 1000; % Motor kv rating.
+kM = I / kV; % [Nm / PWM], motor torque coefficient.
 
-roll_Kp = 1;
-roll_Ki = 1;
-roll_Kd = 1;
+max_voltage = 4; % [V], maximum motor voltage.
+rpm_per_voltage = 1000; % [RPM / V], motor KV rating.
 
-pitch_Kp = 1;
-pitch_Ki = 1;
-pitch_Kd = 1;
+z_Kp = 25;
+z_Ki = 50;
+z_Kd = 0;
+
+roll_Kp = 6;
+roll_Ki = 0.01;
+roll_Kd = 0.25;
+
+pitch_Kp = roll_Kp;
+pitch_Ki = roll_Ki;
+pitch_Kd = roll_Kd;
 
 yaw_Kp = 1;
-yaw_Ki = 1;
-yaw_Kd = 1;
+yaw_Ki = 0;
+yaw_Kd = 0;
